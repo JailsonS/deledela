fe8b5bcd4dce012ed34b8e4b84b118161d263170
JailsonS <engsoaresfilho@gmail.com>
2022-09-30 21:22:34 -0300

    update project

--- app/controllers/home_controller.py
+++ app/controllers/home_controller.py
@@ -5,7 +5,11 @@ class HomeController:
 
     def index(self):
 
-        result = engine.execute('SELECT * FROM TEST_DD.PCCLIENT p WHERE ROWNUM <= 5')
+        result = engine.execute(
+            'SELECT p.CODCLI cod_cli FROM TEST_DD.PCCLIENT p'
+        )
 
         for row in result:
-            print(row)
+            print(row['cod_cli'])
+        
+        return 'ok'
