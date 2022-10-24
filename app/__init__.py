a66882595180a87ca4190857ad48fbcf8c10cdc6
JailsonS <engsoaresfilho@gmail.com>
2022-10-24 13:54:25 -0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from mvc_flask import FlaskMVC
+from flask_session import Session
 
 
 app = Flask(__name__, template_folder='app/templates')
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
 
 FlaskMVC(app)
